835bc28fa59ef56952bb684bc230e6008fe570a2
iriSVakjyrie <seiryuu2772@gmail.com>
2020-08-03 12:43:47 +0900

    ドアの開閉のスクリプト、アニメーションの作成

---
+++ Assets/Scripts/DoorClose.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorClose : MonoBehaviour
+{
+
+    private bool isClose;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClose = false;
+        animator = transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && isClose == false)
+        {
+            Debug.Log("Playerが家に入りました");
+           
+            
+            Debug.Log("ドアが閉まります");
+            animator.SetBool("isOpen", !animator.GetBool("isOpen"));
+            isClose = true;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    private bool isNear;
+    private bool isOpen;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isNear = false;
+        isOpen = false;
+        animator = transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isNear == true && isOpen == false)
+        {
+            Debug.Log("ドアが開きます");
+            animator.SetBool("isOpen", !animator.GetBool("isOpen"));
+            isOpen = true;
+            
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && isOpen == false)
+        {
+            Debug.Log("Playerが判定圏内");
+            Debug.Log("ドアを開けますか？");
+            isNear = true;
+            
+        }
+    }
+}
+
+
