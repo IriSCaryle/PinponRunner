85d8e3809364ae9e0a161411d37b7898f174c9be
iriSVakjyrie <seiryuu2772@gmail.com>
2020-08-06 18:38:38 +0900

    ネームエントリー画面と素材を追加

---
+++ Assets/Scripts/tempNameEntry.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// InputFieldを使えるようにする
+using UnityEngine.UI;
+
+public class tempNameEntry : MonoBehaviour
+{
+
+    // inputfieldを格納する変数
+    InputField inputfield;
+
+    // テキストを格納する変数
+    public Text EntryName;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // InputFieldコンポーネントを格納
+        inputfield = GetComponent<InputField>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+ 
+  
+ 
